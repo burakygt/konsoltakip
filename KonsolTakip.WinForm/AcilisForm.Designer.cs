b34b81cb76e04e98951990a7a9d2ccba16b3c592
gokhanozturk <gokhan_ozturk@hotmail.com>
2014-09-27 23:43:49 +0300

    ggg

--- KonsolTakip.WinForm/AcilisForm.Designer.cs
+++ KonsolTakip.WinForm/AcilisForm.Designer.cs
@@ -87,7 +87,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(12, 178);
+            this.label2.Location = new System.Drawing.Point(12, 135);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(106, 26);
             this.label2.TabIndex = 4;
