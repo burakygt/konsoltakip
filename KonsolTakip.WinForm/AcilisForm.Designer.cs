5bc50bb6b4fb10a566032560642c25623ff3d9c6
Mehmet Şirin Usanmaz <mehmetusanmaz@hotmail.com>
2014-09-28 12:47:57 +0300

    geri aldım mehmet

--- KonsolTakip.WinForm/AcilisForm.Designer.cs
+++ KonsolTakip.WinForm/AcilisForm.Designer.cs
@@ -178,9 +178,9 @@
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(52, 86);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(83, 13);
+            this.label7.Size = new System.Drawing.Size(93, 13);
             this.label7.TabIndex = 14;
-            this.label7.Text = "Burak Değişiklik";
+            this.label7.Text = "Mehmet Değişiklik";
             // 
             // AcilisForm
             // 
